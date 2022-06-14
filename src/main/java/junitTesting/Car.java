adb9b762777f189d58b3d2f6c438b373e78b3a0f
Amila1610 <amilah@comtrade.com>
2022-06-14 15:37:01 +0200

    JUnit testing/ assertEquals

---
+++ src/main/java/junitTesting/Car.java
@@ -0,0 +1,20 @@
+package junitTesting;
+
+public class Car {
+
+    String make;
+    String model;
+    int numDoors;
+    static int wheels;
+
+    public Car(String make, String model, int numDoors){
+        this.make=make;
+        this.model=model;
+        this.numDoors=numDoors;
+    }
+
+    public void printDetails (){
+        System.out.println("Car make " + make + ", car model " + model + ", number of doors  " + numDoors);
+    }
+
+}
---
+++ src/main/java/junitTesting/Toyota.java
@@ -0,0 +1,14 @@
+package junitTesting;
+
+public class Toyota extends Car {
+
+    public Toyota(String make, String model, int numDoors) {
+        super(make, model, numDoors);
+    }
+
+    public void toyotaDetails(){
+        printDetails();
+    }
+
+
+}
